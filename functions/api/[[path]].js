216772e8f2daa4f3a13d5aad76f952307fd56b78
SelfAbandonment <1879816281@qq.com>
2026-02-03 14:24:23 +0800

    添加 Cloudflare Worker 代理解决 HTTPS/HTTP 混合内容问题

---
+++ functions/api/[[path]].js
@@ -0,0 +1,55 @@
+export async function onRequest(context) {
+  const { request } = context;
+  const url = new URL(request.url);
+  
+  // 后端 API 地址
+  const BACKEND_API = 'http://176.97.217.106:8080';
+  const apiPath = url.pathname.replace('/api', '');
+  const apiUrl = `${BACKEND_API}${apiPath}${url.search}`;
+  
+  // 处理 OPTIONS 预检请求
+  if (request.method === 'OPTIONS') {
+    return new Response(null, {
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+        'Access-Control-Allow-Headers': '*',
+      },
+    });
+  }
+  
+  try {
+    // 转发请求到后端
+    const response = await fetch(apiUrl, {
+      method: request.method,
+      headers: request.headers,
+      body: request.method !== 'GET' && request.method !== 'HEAD' 
+        ? await request.arrayBuffer() 
+        : undefined,
+    });
+    
+    const newResponse = new Response(response.body, {
+      status: response.status,
+      statusText: response.statusText,
+      headers: response.headers,
+    });
+    
+    // 添加 CORS 头
+    newResponse.headers.set('Access-Control-Allow-Origin', '*');
+    newResponse.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    newResponse.headers.set('Access-Control-Allow-Headers', '*');
+    
+    return newResponse;
+  } catch (error) {
+    return new Response(JSON.stringify({ 
+      code: 500, 
+      msg: '代理请求失败: ' + error.message 
+    }), {
+      status: 500,
+      headers: { 
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*'
+      }
+    });
+  }
+}
